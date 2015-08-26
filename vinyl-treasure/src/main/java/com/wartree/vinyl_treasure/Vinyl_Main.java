072029088d733f1f333ab2b0fb0002dad030b5cb
Douglas Krieghbaum <Douglas.Krieghbaum@ca.com>
2015-08-26 15:32:21 -0500

    Updating Java

---
+++ vinyl-treasure/src/main/java/com/wartree/vinyl_treasure/Vinyl_Main.java
@@ -0,0 +1,13 @@
+package com.wartree.vinyl_treasure;
+
+/**
+ * Hello world!
+ *
+ */
+public class Vinyl_Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
