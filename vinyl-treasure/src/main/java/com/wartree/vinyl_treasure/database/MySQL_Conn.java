520983911da1f660424036694e45fc52f2f0cf27
Douglas Krieghbaum <Douglas.Krieghbaum@ca.com>
2015-08-31 10:16:36 -0500

    New Structure

---
+++ vinyl-treasure/src/main/java/com/wartree/vinyl_treasure/database/MySQL_Conn.java
@@ -0,0 +1,16 @@
+package com.wartree.vinyl_treasure.database;
+
+/**
+ * Created by krido02 on 8/31/2015.
+ *
+ *
+ * MySQL connection class
+ */
+public class MySQL_Conn {
+
+    public MySQL_Conn() {
+
+        System.out.println( "Connecting to DB" );
+
+    }
+}
