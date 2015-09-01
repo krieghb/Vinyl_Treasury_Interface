fd71e4913b900de7520f0502a2e344a6157ead55
Douglas Krieghbaum <Douglas.Krieghbaum@ca.com>
2015-09-01 15:39:58 -0500

    Fixing value

--- vinyl-treasure/src/main/java/com/wartree/vinyl_treasure/scripts/Script_Constants.java
+++ vinyl-treasure/src/main/java/com/wartree/vinyl_treasure/scripts/Script_Constants.java
@@ -8,70 +8,70 @@ public final class Script_Constants {
 
     public final static String CREATE_DATABASE_VTI = "CREATE DATABASE `v_t_i` /*!40100 DEFAULT CHARACTER SET utf8 */;\n" +
 
-            "CREATE TABLE `v_t_i`.`t_vin-albums` (\n" +
-            "  `t-vin-albums_id` int(11) NOT NULL AUTO_INCREMENT,\n" +
-            "  `t-vin-albums_name` varchar(45) NOT NULL,\n" +
-            "  `t-vin-albums_year` varchar(4) NOT NULL DEFAULT '????',\n" +
-            "  `t-vin-albums_num-songs` int(11) NOT NULL,\n" +
-            "  `t-vin-albums_songs` varchar(45) DEFAULT NULL COMMENT 'placeholder for song list (table)\\n',\n" +
-            "  `t-vin-albums_group` varchar(45) DEFAULT NULL COMMENT 'placeholder for group list (table)',\n" +
-            "  PRIMARY KEY (`t-vin-albums_id`)\n" +
+            "CREATE TABLE `v_t_i`.`t_vin_albums` (\n" +
+            "  `albums_id` int(11) NOT NULL AUTO_INCREMENT,\n" +
+            "  `albums_name` varchar(45) NOT NULL,\n" +
+            "  `albums_year` varchar(4) NOT NULL DEFAULT '????',\n" +
+            "  `albums_numsongs` int(11) NOT NULL,\n" +
+            "  `albums_songs` varchar(45) DEFAULT NULL COMMENT 'placeholder for song list (table)\\n',\n" +
+            "  `albums_group` varchar(45) DEFAULT NULL COMMENT 'placeholder for group list (table)',\n" +
+            "  PRIMARY KEY (`albums_id`)\n" +
             ") ENGINE=InnoDB DEFAULT CHARSET=utf8;\n" +
 
-            "CREATE TABLE `v_t_i`.`t_vin-algroup-ref` (\n" +
-            "  `t-vin-albgrp-ref_albumid` int(11) NOT NULL,\n" +
-            "  `t-vin-albgrp-ref_groupid` int(11) NOT NULL,\n" +
-            "  PRIMARY KEY (`t-vin-albgrp-ref_albumid`,`t-vin-albgrp-ref_groupid`)\n" +
+            "CREATE TABLE `v_t_i`.`t_vin_algroup_ref` (\n" +
+            "  `albgrp_ref_albumid` int(11) NOT NULL,\n" +
+            "  `albgrp_ref_groupid` int(11) NOT NULL,\n" +
+            "  PRIMARY KEY (`albgrp_ref_albumid`,`albgrp_ref_groupid`)\n" +
             ") ENGINE=InnoDB DEFAULT CHARSET=utf8;\n" +
 
-            "CREATE TABLE `v_t_i`.`t_vin-albsng-ref` (\n" +
-            "  `t-vin-albsng-ref_albumid` int(11) NOT NULL,\n" +
-            "  `t-vin-albsng-ref_sngid` int(11) NOT NULL,\n" +
-            "  `t-vin-albsng-ref_sngnum` int(11) NOT NULL,\n" +
-            "  PRIMARY KEY (`t-vin-albsng-ref_albumid`,`t-vin-albsng-ref_sngid`)\n" +
+            "CREATE TABLE `v_t_i`.`t_vin_albsng_ref` (\n" +
+            "  `albsng_ref_albumid` int(11) NOT NULL,\n" +
+            "  `albsng_ref_sngid` int(11) NOT NULL,\n" +
+            "  `albsng_ref_sngnum` int(11) NOT NULL,\n" +
+            "  PRIMARY KEY (`albsng_ref_albumid`,`albsng_ref_sngid`)\n" +
             ") ENGINE=InnoDB DEFAULT CHARSET=utf8;\n" +
 
-            "CREATE TABLE `v_t_i`.`t_vin-comp` (\n" +
-            "  `t-vin-comp_id` int(11) NOT NULL AUTO_INCREMENT,\n" +
-            "  `t-vin-comp_name (last)` varchar(45) NOT NULL,\n" +
-            "  `t-vin-comp_name (first)` varchar(45) DEFAULT NULL,\n" +
-            "  PRIMARY KEY (`t-vin-comp_id`)\n" +
+            "CREATE TABLE `v_t_i`.`t_vin_comp` (\n" +
+            "  `comp_id` int(11) NOT NULL AUTO_INCREMENT,\n" +
+            "  `comp_name (last)` varchar(45) NOT NULL,\n" +
+            "  `comp_name (first)` varchar(45) DEFAULT NULL,\n" +
+            "  PRIMARY KEY (`comp_id`)\n" +
             ") ENGINE=InnoDB DEFAULT CHARSET=utf8;\n" +
 
-            "CREATE TABLE `v_t_i`.`t_vin-group` (\n" +
-            "  `t-vin-group_id` int(11) NOT NULL AUTO_INCREMENT,\n" +
-            "  `t-vin-group_name(last)` varchar(100) NOT NULL DEFAULT 'Various',\n" +
-            "  `t-vin-group_name(first)` varchar(45) DEFAULT NULL,\n" +
-            "  PRIMARY KEY (`t-vin-group_id`)\n" +
+            "CREATE TABLE `v_t_i`.`t_vin_group` (\n" +
+            "  `group_id` int(11) NOT NULL AUTO_INCREMENT,\n" +
+            "  `group_name(last)` varchar(100) NOT NULL DEFAULT 'Various',\n" +
+            "  `group_name(first)` varchar(45) DEFAULT NULL,\n" +
+            "  PRIMARY KEY (`group_id`)\n" +
             ") ENGINE=InnoDB DEFAULT CHARSET=utf8;\n" +
 
-            "CREATE TABLE `v_t_i`.`t_vin-set` (\n" +
-            "  `t-vin-set_id` int(11) NOT NULL AUTO_INCREMENT,\n" +
-            "  `t-vin-set_name` varchar(45) NOT NULL,\n" +
-            "  `t-vin-set_num-albums` int(11) NOT NULL,\n" +
-            "  PRIMARY KEY (`t-vin-set_id`)\n" +
+            "CREATE TABLE `v_t_i`.`t_vin_set` (\n" +
+            "  `set_id` int(11) NOT NULL AUTO_INCREMENT,\n" +
+            "  `set_name` varchar(45) NOT NULL,\n" +
+            "  `set_numalbums` int(11) NOT NULL,\n" +
+            "  PRIMARY KEY (`set_id`)\n" +
             ") ENGINE=InnoDB DEFAULT CHARSET=utf8;\n" +
 
-            "CREATE TABLE `v_t_i`.`t_vin-setal-ref` (\n" +
-            "  `t-vin-setal-ref_setid` int(11) NOT NULL,\n" +
-            "  `t-vin-setal-ref_albumid` int(11) NOT NULL,\n" +
-            "  `t-vin-setal-ref_albumnum` int(11) DEFAULT NULL,\n" +
-            "  PRIMARY KEY (`t-vin-setal-ref_setid`)\n" +
+            "CREATE TABLE `v_t_i`.`t_vin_setal_ref` (\n" +
+            "  `setal_ref_setid` int(11) NOT NULL,\n" +
+            "  `setal_ref_albumid` int(11) NOT NULL,\n" +
+            "  `setal_ref_albumnum` int(11) DEFAULT NULL,\n" +
+            "  PRIMARY KEY (`setal_ref_setid`)\n" +
             ") ENGINE=InnoDB DEFAULT CHARSET=utf8;\n" +
 
-            "CREATE TABLE `v_t_i`.`t_vin-sngcomp-ref` (\n" +
-            "  `t-vin-sngcomp-ref_sngid` int(11) NOT NULL,\n" +
-            "  `t-vin-sng-comp-ref_compid` int(11) NOT NULL,\n" +
-            "  PRIMARY KEY (`t-vin-sngcomp-ref_sngid`,`t-vin-sng-comp-ref_compid`)\n" +
+            "CREATE TABLE `v_t_i`.`sngcomp_ref` (\n" +
+            "  `sngcomp_ref_sngid` int(11) NOT NULL,\n" +
+            "  `sngcomp_ref_compid` int(11) NOT NULL,\n" +
+            "  PRIMARY KEY (`sngcomp_ref_sngid`,`sngcomp_ref_compid`)\n" +
             ") ENGINE=InnoDB DEFAULT CHARSET=utf8;\n" +
 
-            "CREATE TABLE `v_t_i`.`t_vin-song` (\n" +
-            "  `t-vin-song_id` int(11) NOT NULL AUTO_INCREMENT,\n" +
-            "  `t-vin-song_title` varchar(100) NOT NULL,\n" +
-            "  `t-vin-song_duration` varchar(5) DEFAULT '00:00',\n" +
-            "  `t-vin-song_genre` varchar(45) NOT NULL DEFAULT 'Misc',\n" +
-            "  `t-vin-song_comp` varchar(45) DEFAULT NULL COMMENT 'placehoder for composer list (table)',\n" +
-            "  PRIMARY KEY (`t-vin-song_id`)\n" +
+            "CREATE TABLE `v_t_i`.`t_vin_song` (\n" +
+            "  `song_id` int(11) NOT NULL AUTO_INCREMENT,\n" +
+            "  `song_title` varchar(100) NOT NULL,\n" +
+            "  `song_duration` varchar(5) DEFAULT '00:00',\n" +
+            "  `song_genre` varchar(45) NOT NULL DEFAULT 'Misc',\n" +
+            "  `song_comp` varchar(45) DEFAULT NULL COMMENT 'placehoder for composer list (table)',\n" +
+            "  PRIMARY KEY (`song_id`)\n" +
             ") ENGINE=InnoDB DEFAULT CHARSET=utf8;"
 
             ;
